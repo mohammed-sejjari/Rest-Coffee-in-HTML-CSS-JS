41a87c6e825460c7062b0af44edf3c9a4635cbbf
MedSej <sejjarirover@gmail.com>
2025-07-02 21:11:41 +0200

    I added today the part Testimonials

--- script.js
+++ script.js
@@ -9,3 +9,41 @@ menuOpenButton.addEventListener("click", () => {
 
 // Close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click () );
+
+
+// Initiallize Swiper 
+// -------------------
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+    
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+  
+});
+
+// --49:00
